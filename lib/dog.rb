b7e3974219bacd51f78658c86c32473397372fd9
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 15:21:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Dog 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name 
+  end 
+  def name
+    @this_dogs_name 
+  end 
+  def bark 
+    puts "woof!"
+  end
+  fido = Dog.new 
+end 
